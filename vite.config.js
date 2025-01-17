061b3bd39087c9612bad17ed3753391d2570a3a2
benibana2001 <benibana2001@gmail.com>
2025-01-18 01:57:43 +0900

    vercel config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import { defineConfig, transformWithEsbuild } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  define: {
+    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+  },
   plugins: [
     {
       name: "treat-js-files-as-jsx",
