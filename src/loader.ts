a82e634bbb209df88cace0367f30682d2a705a06
benibana2001 <benibana2001@gmail.com>
2025-02-11 20:38:17 +0900

    write type guard about customEvent

--- src/loader.ts
+++ src/loader.ts
@@ -1,15 +1,24 @@
 /****************************************************
  * 通信処理の際に表示するローダー
  ***************************************************/
+
 const loader = function () {
   const elemBlocker = document.querySelector(".blocker") as HTMLElement;
-  document.addEventListener("busy", (event: CustomEvent) => {
+
+  const isCustomEvent = (event: Event): event is CustomEvent => {
+    return "detail" in event;
+  };
+
+  const listner: EventListener = (event: Event) => {
+    if (!isCustomEvent(event)) return;
+
     if (event.detail) {
       elemBlocker.dataset.busy = "true";
     } else {
       elemBlocker.dataset.busy = "false";
     }
-  });
+  };
+  document.addEventListener("busy", listner);
 };
 
 export { loader };
