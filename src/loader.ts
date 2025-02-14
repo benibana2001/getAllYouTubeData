54214e2a2213777b6e5d77a2652770b190b769f1
benibana2001 <benibana2001@gmail.com>
2025-02-14 15:47:15 +0900

    type guard about loading listner

--- src/loader.ts
+++ src/loader.ts
@@ -4,18 +4,19 @@
 
 import { busyStatus } from "./type";
 
-const a = new CustomEvent("click");
-
 class LoaderEvent extends CustomEvent<busyStatus> {
   constructor(type: string, eventInitDict?: CustomEventInit<busyStatus>) {
     super(type, eventInitDict);
   }
 }
 
+
 const loaderInit = function () {
   const elemBlocker = document.querySelector(".blocker") as HTMLElement;
 
-  const listner: EventListener = (event: LoaderEvent) => {
+  const listner: EventListener = (event: Event) => {
+    if(!isLoaderEvent(event)) return;
+
     if (event.detail.isUiLock) {
       elemBlocker.dataset.isshow = "true";
     } else {
@@ -24,5 +25,9 @@ const loaderInit = function () {
   };
   document.addEventListener("busy", listner);
 };
+
+function isLoaderEvent(event: Event): event is LoaderEvent {
+  return event instanceof LoaderEvent 
+}
 
 export { loaderInit, LoaderEvent };
