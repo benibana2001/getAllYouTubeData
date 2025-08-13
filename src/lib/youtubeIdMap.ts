4a6b6c6658ab20289bb556e49758c52f0b375a21
benibana2001 <benibana2001@gmail.com>
2025-08-13 09:10:55 +0900

    add a new file

---
+++ src/lib/youtubeIdMap.ts
@@ -0,0 +1,12 @@
+const youtubeIdMap = {
+  UULF: "Videos",
+  UULP: "Popular_videos",
+  UULV: "Live_streams",
+  UUMF: "Members-only_videos",
+  UUMO: "Members-only_contents",
+  UUMS: "Members-only_short_videos",
+  UUMV: "Members-only_live_streams",
+  UUPS: "Popular_short_videos",
+  UUSH: "Short_videos",
+  UUPV: "Popular_live_streams",
+}
